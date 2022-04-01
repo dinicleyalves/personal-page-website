c4fb19f94044042eb8c433cf09d892f120c5b1bd
Dinicley Alves <dinicleyalvess@gmail.com>
2022-04-01 20:55:05 -0300

    Commit initial

---
+++ 404.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Not Found - 404 </title>
+</head>
+<body>
+    <!-- IMPLEMENTS PAGE 404 ERROR --> 
+    <p>Page not found </p>
+    <h6><a href="./"> Back to home </a></h6>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta name="description" content="Hi, I'm Dinicley Alves, a Developer 🚀 from Brazil.
+	Developer | Technology consultant | Computer teacher | Digital Entrepreneur | Passionate about everything that involves technology">
+	<meta name=”robots” content="index, follow">
+
+  <title> Dinicley Alves - Web Developer </title>
+
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="./css/atropos.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link rel='icon' href="https://img.icons8.com/ios-filled/50/000000/devpost.png"/>
+  
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+</head>
+<body>
+  <span>
+    <img src="./images/faixa.svg" alt="Faixa">
+  </span>
+  <span>
+    <img src="./images/quebra.svg" alt="Faixa">
+  </span>
+
+  <main data-tilt="" data-tilt-glare="true" data-tilt-max-glare="0.1" data-tilt-speed="400" data-tilt-scale="1.02" data-tilt-max="10" data-tilt-perspective="500" style="transform: perspective(500px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1); will-change: transform;">
+    <div class="container">
+
+      <div class="avatar">
+        <img id="userImage" src="https://github.com/dinicleyalves.png" alt="Foto pessoal">
+      </div> 
+
+      <h1 id="userName"> Dinicley Alves </h1>
+      <a id="userLink" href="https://instagram.com/dinicleyalves" target="_blank">
+        <img src="images/github.svg" alt="Ícone Github">
+        <div id="userLogin">dinicleyalves</div>
+      </a>
+
+      <p id="userBio">Hi, I'm Dinicley Alves, a Developer 🚀 from Brazil.</p>
+
+      <ul id="socialLinks">
+        <li class="youtube">
+          <a href="" target="_blank">
+            <img src="./images/youtube.svg" alt="Ícone Youtube" />
+          </a>
+        </li>
+        <li class="linkedin">
+          <a href="" target="_blank">
+            <img src="./images/linkedin.svg" alt="Ícone Linkedin" >
+          </a>
+        </li>
+        <li class="instagram">
+          <a href="" target="_blank">
+            <img src="./images/instagram.svg" alt="Ícone Instagram">
+          </a>
+        </li>
+        <li class="twitch">
+          <a href="" target="_blank">
+            <img src="./images/twitch.svg" alt="Ícone Twitch">
+          </a>
+        </li>
+
+      </ul>
+    </div>
+  </main>
+
+  <script src="./js/vanilla-tilt.js"></script>
+  <script src="./js/main.js"> </script>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="https://wa.me/5594991118291?text=Tudo%20bem%20Dinicley?%20" style="position:fixed;width:100px;height:100px;bottom:40px;right:40px;background-color:#25d366;color:#FFF;border-radius:50px;text-align:center;font-size:30px;box-shadow: 1px 1px 2px #888;
+  z-index:1000;" target="_blank">
+<i style="margin-top:16px" class="fa fa-whatsapp"></i>
+</a>
+</body>
+
+</html>
